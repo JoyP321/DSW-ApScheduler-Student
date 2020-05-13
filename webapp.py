bfaf258bfe443d07ec49677fcca16baa87e0139f
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2020-05-12 20:05:42 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,11 +10,17 @@ app = Flask(__name__)
 #TODO: add the code for the ApScheduler here
 scheduler = BackgroundScheduler({'apscheduler.timezone':'America/Los_Angeles'})
 scheduler.start()
+scheduler.add_job(my_task, tigger= 'interval', minutes=1)
+session['taskCount']= 0
 
- 
 @app.route('/')
 def welcome():
-    return render_template('home.html')
+    return render_template('home.html', content = '')
+ 
+ 
+def my_task():
+    session['taskCount'] = session['taskCount']+1
+    return render_template('home.html', content = 'task count' +str(session['taskCount']))
   
 if __name__=="__main__":
     app.run(debug=False)
